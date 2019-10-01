92f2697a9f8b542abb0c86223a2aac0451bf81ae
MichaelKTHP <michaelkrasnovsky@gmail.com>
2019-10-01 12:09:15 +0200

    Ajout exo 9

---
+++ exo_09.rb
@@ -0,0 +1,5 @@
+puts "Saisissez s'il vous plaît votre prénom"
+user_name = gets.chomp
+puts "Saisissez s'il vous plaît votre nom"
+user_surname = gets.chomp
+puts "Bonjour, #{user_name} #{user_surname} !"
