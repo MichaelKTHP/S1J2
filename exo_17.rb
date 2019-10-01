fcd238812eb9c7d84dfc634a455c69bb739348ed
YoucefKhalainou <youcef_khalainou@yahoo.fr>
2019-10-01 23:09:32 +0200

    Modification du programme

--- exo_17.rb
+++ exo_17.rb
@@ -5,26 +5,24 @@ age = gets.chomp.to_i
 x = age
 y= 0
 
-
-
 age.times do
 	
-	
-
 y= y+1
 x = x-1
 
 #On crée deux variables, x compte l'ecart entre 
 #l'année actuel et l'année itéré dans la boucle  et y compte l'age depuis sa naissance
 
-
-puts" Il y a #{x} ans, tu avais #{y} ans"
+if (y==x) then puts "Il y a #{x} ans, tu avais la moitié de l'âge que tu as aujourd'hui" 
 
 # Pour utiliser if il faut mettre la condition entre parenthèse et utiliser == car un seul = sert
 # à définir une vériable. Ce message n'apparaitra qu'avec un âge pair. 
-if (y==x) then puts "Il y a #{x} ans, tu avais la moitié de l'âge que tu as aujourd'hui" 
 
-	
+else
+
+puts" Il y a #{x} ans, tu avais #{y} ans"	
+
 end
+
 end
 
