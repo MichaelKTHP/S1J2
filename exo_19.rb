18be2249422207079f8f48e5eca0e0dd62973411
MichaelKTHP <michaelkrasnovsky@gmail.com>
2019-10-01 18:01:04 +0200

    ajout exo 19

---
+++ exo_19.rb
@@ -0,0 +1,36 @@
+arrays = []
+
+#Créer un tableau vide
+
+i=1
+#déclarer i=1 pour que dans l'email ça commence avec un 1
+50.times do 
+
+#On fait une itération de 50 fois
+
+if(i<10)
+#On ajoute la condition if pour i entre 1 et 9 afin d'ajouter un zéro à coté de chiffre
+arrays << "jean.dupont.0#{i}@email.fr"
+
+else
+
+arrays << "jean.dupont.#{i}@email.fr"
+end
+
+#On incrémente le i à chaque fois
+i=i+1
+ 
+
+end
+
+i=0
+50.times do
+if (i+1) % 2 == 0
+	puts arrays [i]
+
+end
+i=i+1
+end
+
+ 
+#On affiche la liste des emails
