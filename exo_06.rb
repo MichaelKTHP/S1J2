1d8290d239c49c0bc1ee57699bf06a815e632db5
YoucefKhalainou <youcef_khalainou@yahoo.fr>
2019-10-01 11:06:47 +0200

    Ajout exo_06

---
+++ exo_06.rb
@@ -0,0 +1,8 @@
+number_of_hours_worked_per_day = 10
+number_of_days_worked_per_week = 5
+number_of_weeks_in_THP = 11
+
+puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+
+# Le programme affiche une erreur parce que la variable number_of_minutes_in_an_hour n'est pas déclarée
+puts "Et en minutes ça fait : #{number_of_minutes_in_an_hour * number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
