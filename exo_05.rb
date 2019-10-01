8ae419d36cdc9ac9d12390756c2cc7a5159e1ce7
MichaelKTHP <michaelkrasnovsky@gmail.com>
2019-10-01 10:56:29 +0200

    ajout exo_05.rb

---
+++ exo_05.rb
@@ -0,0 +1,27 @@
+# affiche le texte indiqué
+puts "On va compter le nombre d'heures de travail à THP"
+# Permet d'afficher le résultat du calcul entre {} à la suite du string "Travail :"
+puts "Travail : #{10 * 5 * 11}"
+# Permet d'afficher le résultat du calcul entre {} à la suite du string "En minute ça fait : "
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+# affiche le texte indiqué
+puts "Et en secondes ?"
+# affiche le résultat du calcul indiqué
+puts 10 * 5 * 11 * 60 * 60
+#affiche le texte 
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+#Affiche si le resultat de la comparaison est True ou False (boolén ?)
+puts 3 + 2 < 5 - 7
+# Affiche le résultat du calcul indiqué à la suite du texte indiqué
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+# Affiche le résultat du calcul indiqué à la suite du texte indiqué
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+# Affiche le texte indiqué
+puts "Ok, c'est faux alors !"
+# Affiche le texte indiqué
+puts "C'est drôle ça, faisons-en plus :"
+# Affiche si le resultat de la comparaison indiquée est True ou False à la suite du texte indiqué (idem pour les 2 lignes suivantes)
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
+
